ff39d5726beff80fbcfab50d1aa244be8f587480
jgvictores <jcgvicto@gmail.com>
2018-02-18 18:34:14 +0100

    copy examples of asrob-uc3m/yarp-devices/examples/python

---
+++ examples/python/robotClient.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+import yarp
+import asrob_yarp_devices
+
+yarp.Network.init()
+
+if yarp.Network.checkNetwork() != True:
+    print "[error] Please try running yarp server"
+    quit()
+
+robotOptions = yarp.Property()
+robotOptions.put('device','RobotClient')
+robotOptions.put('name','/ecroSim')
+robotDevice = yarp.PolyDriver(robotOptions)  # calls open -> connects
+
+robot = asrob_yarp_devices.viewIRobotManager(robotDevice)  # view the actual interface
+
+print "moveForward"
+robot.moveForward(1)
+
+print "delay(5)"
+yarp.Time.delay(5)  # delay in [seconds]
+
+print "moveForward"
+robot.stopMovement()
