8e150106bd629a24aac163b2f40d52a205d3803a
Juan G. Victores <jgvictores@users.noreply.github.com>
2018-02-19 19:10:57 +0100

    Update depthClient.m

--- examples/matlab/depthClient.m
+++ examples/matlab/depthClient.m
@@ -3,3 +3,6 @@ LoadYarp;
 input_port = yarp.Port;
 input_port.open("/matlab/ecroSim/depthImage:i");
 yarp.Network.connect("/ecroSim/depthImage:o", "/matlab/ecroSim/depthImage:i");
+
+height = 96;
+width = 128;
