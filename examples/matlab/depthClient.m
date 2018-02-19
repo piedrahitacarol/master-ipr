8b4185d3a1021ca2b43049ae9476a9e91b9c5e70
jgvictores <jcgvicto@gmail.com>
2018-02-19 23:31:00 +0100

    improve depthClient.m

--- examples/matlab/depthClient.m
+++ examples/matlab/depthClient.m
@@ -1,16 +1,18 @@
 LoadYarp;  % yarp.matlab.LoadYarp
 
-input_port = yarp.BufferedPortImageFloat;
-input_port.open("/matlab/ecroSim/depthImage:i");
-yarp.Network.connect("/ecroSim/depthImage:o", "/matlab/ecroSim/depthImage:i");
+inputPort = yarp.BufferedPortImageFloat;
+inputPort.open('/matlab/ecroSim/depthImage:i');
+yarp.Network.connect('/ecroSim/depthImage:o', '/matlab/ecroSim/depthImage:i');
 
-yarpImage=input_port.read;
-h=yarpImage.height;
-w=yarpImage.width;
+yarpImage = inputPort.read;
+h = yarpImage.height;
+w = yarpImage.width;
 %tool to convert the yarpImage (a Java object) into a matlab matrix
-tool=YarpImageHelper(h, w);  % yarp.matlab.YarpImageHelper
-A=tool.get2DMatrix(yarpImage);
+tool = YarpImageHelper(h, w);  % yarp.matlab.YarpImageHelper
+image = tool.get2DMatrix(yarpImage);
 %norm to visualize
-normA = A - min(A(:));
-normA = normA ./ max(normA(:)); % *
-imshow(normA)
+normImage = image - min(image(:));
+normImage = normImage ./ max(normImage(:)); % *
+imshow(normImage);
+
+inputPort.close();
