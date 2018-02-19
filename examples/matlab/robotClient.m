c9fbb0262eaec2c1072c62e61718610c3bf2fd21
jgvictores <jcgvicto@gmail.com>
2018-02-19 22:16:17 +0100

    working robot move

--- examples/matlab/robotClient.m
+++ examples/matlab/robotClient.m
@@ -1,1 +1,11 @@
-LoadYarp;
+LoadYarp;  % yarp.matlab.LoadYarp
+
+port = yarp.Port;
+port.open('/matlab/ecroSim/rpc:c');
+yarp.Network.connect('/matlab/ecroSim/rpc:c','/ecroSim/rpc:s');
+
+b = yarp.Bottle;
+b.addVocab(yarp.Vocab.encode('movf'));
+b.addDouble(3.0);
+port.write(b);
+
