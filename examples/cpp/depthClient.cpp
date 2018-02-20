9aba6d844450c8e2d5d3e9885bd0bb3b6994809c
jgvictores <jcgvicto@gmail.com>
2018-02-20 15:13:16 +0100

    complete depth cpp

--- examples/cpp/depthClient.cpp
+++ examples/cpp/depthClient.cpp
@@ -11,9 +11,18 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    yarp::os::Port port;
+    yarp::os::BufferedPort<yarp::sig::ImageOf< yarp::sig::PixelFloat >> port;
     port.open("/cpp/ecroSim/depthImage:i");
     yarp::os::Network::connect("/ecroSim/depthImage:o","/cpp/ecroSim/depthImage:i");
+
+    yarp::sig::ImageOf< yarp::sig::PixelFloat>* yarpImage = port.read(true);  // true makes blocking
+
+    printf("Got image: height %d, width %d\n",yarpImage->height(),yarpImage->width());
+
+    printf("Pixel (0,0): %f [mm] depth\n",yarpImage->pixel(0,0));
+
+    // For OpenCV example see:
+    // https://github.com/robotology/yarp/blob/master/example/opencv/main.cpp
 
     port.close();
 
