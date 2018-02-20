62c9e6834cc50108cef87a73120f95a3055edb42
jgvictores <jcgvicto@gmail.com>
2018-02-20 15:05:17 +0100

    change to port

--- examples/cpp/depthClient.cpp
+++ examples/cpp/depthClient.cpp
@@ -11,50 +11,11 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    /*yarp::os::Property robotOptions;
-    robotOptions.put("device", "RobotClient"); // our device (a dynamically loaded library)
-    robotOptions.put("name", "/ecroSim"); // remote port through which we'll talk to the server
+    yarp::os::Port port;
+    port.open("/cpp/ecroSim/depthImage:i");
+    yarp::os::Network::connect("/ecroSim/depthImage:o","/cpp/ecroSim/depthImage:i");
 
-    yarp::dev::PolyDriver robotDevice;
-    if ( ! robotDevice.open(robotOptions) )
-    {
-        printf("Device not available.\n");
-        robotDevice.close();
-        return 1;
-    }
-
-    asrob::IRobotManager *robot;
-
-    if ( ! robotDevice.view(robot) )
-    {
-        printf("[error] Problems acquiring interface\n");
-        robotDevice.close();
-        return 1;
-    }
-    printf("[success] acquired motor interface\n");
-
-    printf("moveForward(3)\n");
-    robot->moveForward(3);
-
-    printf("delay(3)\n");
-    yarp::os::Time::delay(3);  // delay in [seconds]
-
-    printf("robot.turnRight(3)\n");
-    robot->turnRight(3);
-
-    printf("delay(5)\n");
-    yarp::os::Time::delay(5);  // delay in [seconds]
-
-    printf("moveForward(3)\n");
-    robot->moveForward(3);
-
-    printf("delay(3)\n");
-    yarp::os::Time::delay(3);  // delay in [seconds]
-
-    printf("stopMovement()\n");
-    robot->stopMovement();
-
-    robotDevice.close();*/
+    port.close();
 
     return 0;
 }
