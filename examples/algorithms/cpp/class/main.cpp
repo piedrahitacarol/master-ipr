17ce61e5dd71a0b84eb8256ce68575c0205fbbd1
jgvictores <jcgvicto@gmail.com>
2018-03-20 18:06:20 +0100

    begin

--- examples/algorithms/cpp/class/main.cpp
+++ examples/algorithms/cpp/class/main.cpp
@@ -1,5 +1,34 @@
+#include <vector>
+#include <fstream>
+
+
+class Node
+{
+public:
+    Node(int x,int y,int id,int parentId) {
+      _x = x; _y = y;
+      _id = id; _parentId = parentId;
+    }
+
+    int _x,_y,_id,_parentId;
+};
+
+class Program
+{
+public:
+    bool run()
+    {
+        return true;
+    }
+
+private:
+    std::vector<Node*> nodes;
+};
 
 int main()
 {
+    Program program;
+    if( ! program.run() )
+        return 1;
     return 0;
 }
