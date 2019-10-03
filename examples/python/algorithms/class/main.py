885727b8ac1f39d4538d1b5e865a7afcdc3eac8d
Juan G. Victores <jgvictores@users.noreply.github.com>
2019-10-03 20:25:30 +0200

    Update main.py

--- examples/python/algorithms/class/main.py
+++ examples/python/algorithms/class/main.py
@@ -51,4 +51,61 @@ while not done:
     for node in nodes:
         node.dump()
 
-    break
+        # up
+        tmpX = node.x - 1
+        tmpY = node.y
+        if( charMap[tmpX][tmpY] == '4' ):
+            print("up: GOALLLL!!!")
+            goalParentId = node.myId
+            done = True
+            break
+        elif ( charMap[tmpX][tmpY] == '0' ):
+            print("up: mark visited")
+            newNode = Node(tmpX, tmpY, len(nodes), node.myId)
+            charMap[tmpX][tmpY] = '2'
+            nodes.append(newNode)
+
+        # down
+        tmpX = node.x + 1
+        tmpY = node.y
+        if( charMap[tmpX][tmpY] == '4' ):
+            print("down: GOALLLL!!!")
+            goalParentId = node.myId
+            done = True
+            break
+        elif ( charMap[tmpX][tmpY] == '0' ):
+            print("down: mark visited")
+            newNode = Node(tmpX, tmpY, len(nodes), node.myId)
+            charMap[tmpX][tmpY] = '2'
+            nodes.append(newNode)
+
+        # right
+        tmpX = node.x
+        tmpY = node.y + 1
+        if( charMap[tmpX][tmpY] == '4' ):
+            print("right: GOALLLL!!!")
+            goalParentId = node.myId
+            done = True
+            break
+        elif ( charMap[tmpX][tmpY] == '0' ):
+            print("right    : mark visited")
+            newNode = Node(tmpX, tmpY, len(nodes), node.myId)
+            charMap[tmpX][tmpY] = '2'
+            nodes.append(newNode)
+
+        # left
+        tmpX = node.x
+        tmpY = node.y - 1
+        if( charMap[tmpX][tmpY] == '4' ):
+            print("left: GOALLLL!!!")
+            goalParentId = node.myId
+            done = True
+            break
+        elif ( charMap[tmpX][tmpY] == '0' ):
+            print("left: mark visited")
+            newNode = Node(tmpX, tmpY, len(nodes), node.myId)
+            charMap[tmpX][tmpY] = '2'
+            nodes.append(newNode)
+
+        dumpMap()
+        
